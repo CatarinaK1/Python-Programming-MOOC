23f7b9d92d102e981195fa40305003c18d0d3af1
Catarina Kaucher <111956032+CatarinaK1@users.noreply.github.com>
2023-11-12 16:57:13 +0200

    Create Age_of-maturity.py

---
+++ Part 2 - Conditionals/Age_of-maturity.py
@@ -0,0 +1,15 @@
+#Please write a program which 
+#asks the user for their age. 
+#The program should then 
+#print out a message based on whether the user is of age or not, 
+#using 18 as the age of maturity.
+
+
+# Write your solution here
+age = int(input("How old are you? "))
+
+if age >= 18:
+    print("You are of age!")
+
+else:
+    print("You are not of age!")
