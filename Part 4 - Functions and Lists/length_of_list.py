d522bbba70eeecacdae4bcf301039e699db70bdf
Catarina Kaucher <111956032+CatarinaK1@users.noreply.github.com>
2023-12-03 12:58:16 +0200

    Add files via upload

---
+++ Part 4 - Functions and Lists/length_of_list.py
@@ -0,0 +1,15 @@
+# Write your solution here
+def length(my_list):
+
+    list_length = len(my_list)
+
+    return list_length
+
+
+
+
+# You can test your function by calling it within the following block
+if __name__ == "__main__":
+    my_list = [3, 6, -4]
+    result = length(my_list)
+    print(result)
