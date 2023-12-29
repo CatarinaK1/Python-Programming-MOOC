8d494cbb599ce443466fef9e1462f0b4bb2c8041
Catarina Kaucher <111956032+CatarinaK1@users.noreply.github.com>
2023-12-29 15:30:26 +0200

    Add files via upload

---
+++ Part 5 - Lists_Dictionary_Tuple/sudoku_column.py
@@ -0,0 +1,36 @@
+def column_correct(sudoku: list, column_no: int) -> bool:
+
+    column_count = []
+
+    for column in sudoku:
+        
+            #item = column[column_no]
+            column_count.append(column[column_no])
+
+
+
+    for i in column_count:
+        if column_count.count(i) > 1 and i != 0:
+            return False
+        
+    return True
+
+
+
+
+
+if __name__ == "__main__":
+    sudoku = [
+  [9, 0, 0, 0, 8, 0, 3, 0, 0],
+  [2, 0, 0, 2, 5, 0, 7, 0, 0],
+  [0, 2, 0, 3, 0, 0, 0, 0, 4],
+  [2, 9, 4, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 7, 3, 0, 5, 6, 0],
+  [7, 0, 5, 0, 6, 0, 4, 0, 0],
+  [0, 0, 7, 8, 0, 3, 9, 0, 0],
+  [0, 0, 1, 0, 0, 0, 0, 0, 3],
+  [3, 0, 0, 0, 0, 0, 0, 0, 2]
+    ]
+
+    print(column_correct(sudoku, 0))
+    print(column_correct(sudoku, 1))
