73e1fa67575f7847e0298edc73fedda607d7bfcd
Catarina Kaucher <111956032+CatarinaK1@users.noreply.github.com>
2024-01-14 15:47:09 +0200

    Add files via upload

---
+++ Part 4 - Functions and Lists/list_of_stars.py
@@ -0,0 +1,18 @@
+# Write your solution here
+def list_of_stars(Star_List: list):
+
+    for i in Star_List:
+
+        print("*" * i)
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    Star_List = [2,1,4,1,6,8,9]
+    list_of_stars(Star_List)
