fd264452b34d8b1646074d472aa0b144036e3183
Catarina Kaucher <111956032+CatarinaK1@users.noreply.github.com>
2024-03-02 20:38:53 +0200

    Update fruit_market.py

--- Part 6 - File_Handling/Fruit Market/fruit_market.py
+++ Part 6 - File_Handling/Fruit Market/fruit_market.py
@@ -1,1 +1,16 @@
-s
+def read_fruits() -> dict:
+
+    fruits_dictionary = {}
+
+    with open("fruits.csv") as fruits:
+        for line in fruits:
+
+            line = line.replace("\n", "")
+            fruitItem = line.split(";")
+
+            fruits_dictionary[fruitItem[0]] = float(fruitItem[1])
+
+
+    return fruits_dictionary
+
+
